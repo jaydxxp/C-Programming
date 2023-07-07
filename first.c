98d408fa7318426a3f92c789a3ebb3a1d5bc44d2
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-07 21:09:37 +0530

    latest commit

---
+++ first.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b;
+  printf("enter 1st number:");
+  scanf("%d",&a);
+
+  printf("enter 2nd number:");
+  scanf("%d",&b);
+
+  if(a == b)
+  {
+    printf("a is equal to b");
+  }
+  else
+  {
+    printf("a is not equal to b");
+  }
+  
+ return 0;
+}
