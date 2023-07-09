9e3e4ab2672dfe11f8f39ab9e0f0da3b95688c1a
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-09 15:38:29 +0530

    area of square in which side is entered by user.

---
+++ CHAP1_P2.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //area of square
+    float side;
+    printf("enter side:");
+    scanf("%f",&side);
+
+    printf("area of square is %f", side*side);
+
+    
+    return 0;
+
+}
