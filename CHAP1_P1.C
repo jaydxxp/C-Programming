9024c13cf74c16187d27b160b164b1b6c40276c5
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-09 15:37:43 +0530

    addition of 2 numbers from a user.

---
+++ CHAP1_P1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    
+    // The addition of two numbers
+    int a,b;
+    printf("enter number 1:");
+    scanf("%d",&a);
+
+    printf("enter number 2:");
+    scanf("%d",&b);
+
+    printf("sum is %d",a+b);
+    return 0;
+
+}
