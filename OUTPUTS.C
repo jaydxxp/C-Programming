fbc8a169de52dfeba23e93eb809de0b1c1b5e01d
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-09 15:33:42 +0530

    outputs in c

---
+++ OUTPUTS.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{   //for integers
+
+    int age=22;
+    printf("my age is %d \n",age);
+
+    //for characters
+    
+    char star='*';
+    printf("star symbol is %c \n",star);
+
+    //for decimals
+
+    float pi=3.14;
+    printf("pi value is %f \n",pi);
+
+    return 0;
+
+}
