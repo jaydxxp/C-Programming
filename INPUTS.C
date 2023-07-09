c9aa11c3b18144a3de894476e824072d446596d6
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-09 15:36:13 +0530

    Input from user.

---
+++ INPUTS.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{   
+
+    // for integers
+    int age;
+    printf("enter age:");
+    scanf("%d",&age);
+    printf("age is %d \n",age);
+
+
+    // for decimals
+    float pi;
+    printf("\nenter value of pi: ");
+    scanf("%f",&pi);
+    printf("value of pi is %f",pi);
+
+    
+    //for characters
+    char star;
+    printf("\n how star look like:");
+    scanf("%c",&star);
+    printf("symbol of star is %c",star);
+
+
+return 0;
+}
