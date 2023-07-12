d114f760fdb7a41b214e87b0aa3397d8faffe0d0
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-12 23:08:02 +0530

    greater less condition

---
+++ RELATIONALOP.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    //relational operators.
+
+    //equal to.
+    printf("%d \n",4==4);
+
+    //greater than or equal to.
+    printf("%d \n",4>=3);
+
+    //less than or equal to.
+    printf("%d \n",5<=4);
+
+    //not equal to.
+    printf("%d \n",5!=5);
+
+
+    return 0;
+
+}
