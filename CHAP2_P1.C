a091cb66d773e64e213d874db8f5e1d8d616507e
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-12 23:13:40 +0530

    program to check number is divisible by 2 or not.

---
+++ CHAP2_P1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    //program to check if a number is divible by 2 or not.
+    int a;
+    printf("enter number:");
+    scanf("%d",&a);
+    printf("%d",a%2==0 );
+
+
+    return 0;
+}
