af3e25c4eb76120d8f04d7b54171b32203adc9c7
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-12 23:11:25 +0530

    logical terms used.

---
+++ LOGICALOP.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    //Logical operator
+
+    //AND(&&)
+    printf("%d \n", 3<4 && 4>3);
+
+    //OR(||)
+    printf("%d \n",3<2 || 3>4);
+
+    //NOT(!)
+    printf("%d \n", !(3<2 || 3>4));
+
+
+    return 0;
+
+}
