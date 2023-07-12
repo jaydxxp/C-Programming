1a5bccb0fb652feed888d02229ebe6a8c87a40b9
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-12 23:05:08 +0530

    two variable type results.

---
+++ TYPECONVERSION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    //int op int
+    printf("%d\n",2*2);
+
+    //int op float
+    printf("%f \n",2.0*2);
+    
+    //float op float
+    printf("%f",2.0*2.0);
+    return 0;
+
+}
