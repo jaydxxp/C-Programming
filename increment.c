baa91b7a97ec1324a8f9118fb2463a8963a03208
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-17 01:45:55 +0530

    increment and decrement.

---
+++ increment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //i++
+    /*int i=1;
+    printf("%d\n",i++); //use i then addition.
+    printf("%d\n",i);*/
+    //++i
+    /*int j=1;
+    printf("%d\n",++j); //add then j value prints.
+    return 0;*/
+    /*int k=1;
+    printf("%d \n",k--); //prints k then substract.
+    printf("%d\n",k); */
+    
+    int m=1;
+    printf("%d \n",--m);
+    printf("%d \n",m); //substract then print m.
+}
