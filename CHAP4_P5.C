903794e77c070ebe07b06f51dc268b4afefd7a92
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-17 01:55:16 +0530

    print of all odd numbers from 5 to 50.

---
+++ CHAP4_P5.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    //print all odd numbers from 5 to 50
+    for(int i=5; i<=50; i++)
+    {
+        if(i%2==0)
+        {
+            continue;
+        }
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
