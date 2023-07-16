9e4983470540983df6fb8b2922b8de5757078229
jaydxxp <138896302+jaydxxp@users.noreply.github.com>
2023-07-17 01:58:37 +0530

    format of if else condition.

---
+++ ifelse_format.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    /*if()
+     //condition in brackets.(circular bracket)
+    {
+       //if condition true than the printable text here.
+    }
+    else
+    {
+        //if condition false than the printable text here.
+    }*/
+
+    return 0;
+}
